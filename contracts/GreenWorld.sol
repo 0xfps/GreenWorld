05ba589d75eb5dd65c514afd641d8e54eb34cebf
fps <anthony.nnaemeka.umeh@gmail.com>
2022-04-04 23:29:49 +0100

    Update GreenWorld.sol

--- contracts/GreenWorld.sol
+++ contracts/GreenWorld.sol
@@ -1,42 +1,106 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
- 
+
+
+
 abstract contract Context {
+
+    // {_msgSender()} Returns the payable address of the caller i.e `msg.sender`.
+    
     function _msgSender() internal view virtual returns (address payable) {
         return payable(msg.sender);
     }
+ 
+ 
+ 
+ 
+    // {_msgData()} Returns transaction data contained in the `msg.data`.
  
     function _msgData() internal view virtual returns (bytes memory) {
         this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
         return msg.data;
     }
 }
- 
+
+
+
+
 abstract contract Ownable is Context {
+
     address private _owner;
+ 
+ 
+    // Event {OwnershipTransferred} called on contract creation or whenever ownership of the contract is transferred.
  
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
  
+ 
+ 
+ 
     constructor () {
+    
+        // Fetch the deployer of the contract from the {_msgSender()} function and assign it to address `msgSender`.
+    
         address msgSender = _msgSender();
+	
+	
+	// Makes this address above the owner, and emits the {OwnershipTransferred} event.
+	
         _owner = msgSender;
+	
         emit OwnershipTransferred(address(0), msgSender);
     }
  
+ 
+ 
+ 
+    // Returns the current owner of the contract from the address variable `owner`.
+    
     function owner() public view virtual returns (address) {
         return _owner;
     }
  
+ 
+ 
+ 
+    // Modifier {onlOwner()} makes sure that the current caller of the modified function is the current owner address `_owner` of the contract.
+    
     modifier onlyOwner() {
         require(owner() == _msgSender(), "Ownable: caller is not the owner");
         _;
     }
  
+ 
+ 
+ 
+    /*
+    * @dev:
+    * Emits the {OwnershipTransferred()} event.
+    *
+    * {renounceOwnership()}, removes the right to the ownership of the contract from the `_owner`.
+    *
+    * Sets the owner to a zero address;
+    */
+    
     function renounceOwnership() public virtual onlyOwner {
         emit OwnershipTransferred(_owner, address(0));
         _owner = address(0);
     }
  
+ 
+ 
+ 
+    /*
+    * @dev:
+    * {transferOwnership()}, transfers the ownership of the contract to `newOwner`.
+    *
+    * New address `newOwner` cannot be a zero address.
+    *
+    * This can only be called by the owner of the contract.
+    *
+    * Emits the {OwnershipTransferred()} event.
+    */
+    
     function transferOwnership(address newOwner) public virtual onlyOwner {
         require(newOwner != address(0), "Ownable: new owner is the zero address");
         emit OwnershipTransferred(_owner, newOwner);
